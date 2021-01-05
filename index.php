cbb0f5affd78660b154daeb499f81e1c4f47219b
flrobson77 <rferreira@ifsp.edu.br>
2021-01-05 00:43:42 -0300

    Ticket 01 - 05012021 - inicio

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+    <meta charset="UTF-8">
+    <title> SHINE Modas Corp. </title>
+    <meta name="description" content="Essa página apresenta a Shinemodas como uma grande loja de roupas plus size"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <!--
+        FAVICON
+        STYLE
+        FONT
+    -->
+    </head>
+
+    <body>
+        <header>
+            <img title="Logomarca" alt="[Logomarca]" src="./_imagens/tux.jpg"/>
+            <h1> SHINE Modas Corp. </h1>
+
+            <ul>
+            <li><a href="#home" title="Shinemodas">Home</a></li>
+            <li><a href="#sobre" title="Sobre a Shinemodas">Sobre</a></li>
+            <li><a target="_blank" rel="nofollow" href="./produtos.php" title="Conheca os produtos da Shinemodas">Produtos</a></li>
+            <li><a href="#contato" title="Contate a Shinemodas">Contato</a></li>
+            </ul>
+        </header>
+
+        <main>
+        <h1> Olá mundo </h1>
+
+        </main>
+
+        <footer>
+        <h1>Shinemodas Corp.</h1>
+        <h2>Segurança da Informação</h2>
+        <h2>SecurityLabs<h2>
+        <ul>
+            <li><a target="_blank" title="twitter da SecurityLabs" href="//twitter.com/flrobson"><img title="twitter da securitylabs" alt="twitter da securitylabs" src="./_imagens/twrlogo.jpg"/></a>
+            <li><a target="_blank" title="Perfil da SecurityLabs no Facebook" href="//facebook.com/slprofrobson"><img title="Pagina da securitylabs no facebook" alt="Pagina da securitylabs no facebook" src="./_imagens/fbklogo.jpg"/></a>
+        </ul>
+        </footer>
+
+    </body>
+</html>
