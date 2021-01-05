eb5e6e5160cfc76c0bdc8dd2fb7cb19ef2975a54
flrobson77 <rferreira@ifsp.edu.br>
2021-01-05 01:41:47 -0300

    ticket03 - 05012021 - Semantica da pagina index

--- index.php
+++ index.php
@@ -15,31 +15,43 @@
 
     <body>
         <header>
-            <img title="Logomarca" alt="[Logomarca]" src="./_imagens/tux.jpg"/>
-            <h1> SHINE Modas Corp. </h1>
-
+            <img title="Logomarca" alt="[Logomarca]" src="./_imagens/logoshine.png"/>
             <ul>
-            <li><a href="#home" title="Shinemodas">Home</a></li>
-            <li><a href="#sobre" title="Sobre a Shinemodas">Sobre</a></li>
-            <li><a target="_blank" rel="nofollow" href="./produtos.php" title="Conheca os produtos da Shinemodas">Produtos</a></li>
-            <li><a href="#contato" title="Contate a Shinemodas">Contato</a></li>
+                <li><a href="#home" title="Shinemodas">Home</a></li>
+                <li><a href="#sobre" title="Sobre a Shinemodas">Sobre</a></li>
+                <li><a target="_blank" rel="nofollow" href="./produtos.php" title="Conheca os produtos da Shinemodas">Produtos</a></li>
+                <li><a href="#contato" title="Contate a Shinemodas">Contato</a></li>
             </ul>
         </header>
 
         <main>
-        <h1> Olá mundo </h1>
-
+        <h1> Moda Plus Size </h1>
+        <img title="Foto_Capa_01" alt="[Foto_Capa_01]" src="./_imagens/people_05.jpg"/>
+        <p>
+        O termo, “plus size” é destinado aos modelos de roupa acima de 48 de calça para cima de GG em diante para blusas.
+        Para a moda social masculina fica em manequim a partir do tamanho 5 de camisa, seja ela manga curta ou comprimida.
+        É uma categorização da moda para organizar melhor suas categorias e não outro estilo de se vestir.
+        </p>
+        <img title="Foto_Capa_02" alt="[Foto_Capa_02]" src="./_imagens/people_03.jpg"/>
+        <p>
+        Segundo Isabella Potter do portal ONDM a moda Plus Size vem cada vez mais ganhando força no Brasil!<br/> 
+        </p>
+        <p>
+        O termo Plus Size foi criado na década de 70 nos Estados Unidos, dentro da própria indústria de moda, para designar os
+        manequins acima de 44, porém, começou a ganhar conhecimento na década de 90. As mulheres que são consideradas Plus Size
+        atualmente, já foram o ideal de corpo feminino em diversos períodos históricos. Apesar de muitas pessoas falarem que estar
+        acima do peso faz mal para a saúde, a moda plus size não surgiu para incentivar o excesso de peso e sim, para valorizar a
+        diversidade e a curva dos corpos, indo contra os padrões de beleza e a ditadura da magreza.    
+        </p>
         </main>
 
         <footer>
-        <h1>Shinemodas Corp.</h1>
-        <h2>Segurança da Informação</h2>
-        <h2>SecurityLabs<h2>
+        <h1>Shine Modas Corp.</h1>
+        <h3>Democracia na moda com estilo e elegância</h3>
         <ul>
-            <li><a target="_blank" title="twitter da SecurityLabs" href="//twitter.com/flrobson"><img title="twitter da securitylabs" alt="twitter da securitylabs" src="./_imagens/twrlogo.jpg"/></a>
-            <li><a target="_blank" title="Perfil da SecurityLabs no Facebook" href="//facebook.com/slprofrobson"><img title="Pagina da securitylabs no facebook" alt="Pagina da securitylabs no facebook" src="./_imagens/fbklogo.jpg"/></a>
+            <li><a target="_blank" title="twitter da Shinemodas" href="//twitter.com/shinemodas"><img title="twitter da securitylabs" alt="twitter da shinemodas" src="./_imagens/twrlogo.jpg"/></a>
+            <li><a target="_blank" title="Perfil da ShineModas no Facebook" href="//facebook.com/shinemodas"><img title="Pagina da securitylabs no facebook" alt="Pagina da shinemodas no facebook" src="./_imagens/fbklogo.jpg"/></a>
         </ul>
         </footer>
-
     </body>
 </html>
